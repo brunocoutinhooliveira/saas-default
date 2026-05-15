1fb13aed6696678665221504fc82ba66c5ffbd7e
Bruno Coutinho <brunodba@live.com>
2026-05-15 08:16:18 -0300

    Tenant Model Configured.

---
+++ app/Models/Tenant.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Stancl\Tenancy\Contracts\TenantWithDatabase;
+use Stancl\Tenancy\Database\Concerns\HasDatabase;
+use Stancl\Tenancy\Database\Concerns\HasDomains;
+use Stancl\Tenancy\Database\Models\Tenant as BaseTenant;
+
+class Tenant extends BaseTenant implements TenantWithDatabase
+{
+    use HasDatabase;
+    use HasDomains;
+
+    public static function getCustomColumns(): array
+    {
+        return [
+            'id',
+            'name',
+            'slug',
+            'plan_id',
+            'is_active',
+        ];
+    }
+}

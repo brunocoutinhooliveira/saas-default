0fa4419b5baf9991113c210e38dc890afc39c95b
Bruno Coutinho <brunodba@live.com>
2026-05-15 08:26:51 -0300

    Migrations: Plans and Tenants Created and Updated.

---
+++ database/migrations/2026_05_15_112251_add_columns_to_tenants_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('tenants', function (Blueprint $table) {
+            $table->string('name')->nullable();
+            $table->string('slug')->nullable()->unique();
+            $table->foreignId('plan_id')->nullable()->constrained('plans')->nullOnDelete();
+            $table->boolean('is_active')->default(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tenants', function (Blueprint $table) {
+            $table->dropColumn(['name', 'slug', 'plan_id', 'is_active']);
+        });
+    }
+};
